70a91d7ebc3f7ab576f47817284be3e7adbacf60
Orange Group Tech <orangemanagementcorpn@gmail.com>
2020-09-19 08:36:23 +0800

    Initial Commit

---
+++ tester.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  readline.question('Please input your token.\n', tokencollected => {
+    const token = tokencollected
+    readline.close();
+    client.login(token)
+    console.log('Validating token. If this takes a long time it means token is not valid.')
+client.once('ready',() => {
+    console.log('This token is valid. \nCan\'t login with this token even though it\'s valid? \nTry reinstalling Discord.js using npm i discord.js')
+    process.exit();
+})
+process.on('unhandledRejection',() => {
+  console.log('This token is not valid. \nMake sure it\'s copied correctly and try again.')
+  process.exit()
+})
+  });
