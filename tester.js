9d5d51d26b7a934ff4ea671e323f0eb97d49a200
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2020-11-04 17:05:38 -0800

    Update tester.js

--- tester.js
+++ tester.js
@@ -9,6 +9,10 @@ const readline = require('readline').createInterface({
     const token = tokencollected
     readline.close();
     client.login(token)
+    .catch(err => {
+      console.log('This token is not valid. \nMake sure it\'s copied correctly and try again.')
+      process.exit()
+    })
     console.log('Validating token. If this takes a long time it means token is not valid.')
 client.once('ready',() => {
     console.log('This token is valid. \nCan\'t login with this token even though it\'s valid? \nTry reinstalling Discord.js using npm i discord.js')
@@ -18,4 +22,5 @@ process.on('unhandledRejection',() => {
   console.log('This token is not valid. \nMake sure it\'s copied correctly and try again.')
   process.exit()
 })
+//This shouldn't happen.
   });
